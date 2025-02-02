52532b938f23e326d990457449a5fdad01050799
Silk Rose <silkrose@love-tolerance.com>
2025-02-01 19:38:44 -0500

    Add blog embeds

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use actix_cors::Cors;
 use actix_web::web::Data;
 use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 use chrono::Local;
+use pony::fimfiction_api::blog::BlogApi;
 use pony::fimfiction_api::story::StoryApi;
 use pony::fimfiction_api::user::UserApi;
 use regex::Regex;
@@ -67,6 +68,18 @@ struct User {
 	timestamp: u128,
 	bio_bbcode: String,
 	profile_pic_256_url: Option<String>,
+}
+
+#[derive(Debug, Clone)]
+struct Blog {
+	link: String,
+	title: String,
+	requests: u32,
+	author_id: u32,
+	timestamp: u128,
+	story_id: Option<u32>,
+	content_bbcode: String,
+	date_published: String,
 }
 
 #[get("/story/{id:.*}")]
@@ -172,27 +185,51 @@ async fn get_user(
 		drop(users);
 		let local_time = Local::now().format("%Y-%m-%d %H:%M:%S").to_string();
 		println!("{local_time}: [user]  cache miss: {ident}");
-		let fimfic = format!("https://www.fimfiction.net/api/v2/users/{ident}");
-		let response = handle_request(&api, &fimfic).await.unwrap();
-		let api = response.json::<UserApi>().await.unwrap();
-		let image = (!api.data.attributes.avatar.r64.ends_with("none_64.png"))
-			.then_some(api.data.attributes.avatar.r256);
-		let re = LazyLock::new(|| Regex::new(r"\[[^]]+\]").unwrap());
-		let user = User {
-			link: api.data.meta.url,
-			name: api.data.attributes.name.replace('"', "&quot;"),
-			requests: 1,
-			color: api.data.attributes.color.hex,
-			timestamp: unix_time().unwrap(),
-			bio_bbcode: re
-				.replace_all(&api.data.attributes.bio, "")
-				.to_string()
-				.replace('"', "&quot;"),
-			profile_pic_256_url: image,
-		};
+		let user = request_user(ident, api).await?;
 		Ok(HttpResponse::Ok()
 			.content_type("text/html; charset=utf-8")
 			.body(user_template(&user)))
+	}
+}
+
+#[get("/blog/{id:.*}")]
+async fn get_blog(
+	path: web::Path<String>, api: web::Data<Arc<FimficRequest>>,
+	blog_data: web::Data<Arc<RwLock<HashMap<u32, Blog>>>>,
+	user_data: web::Data<Arc<RwLock<HashMap<u32, User>>>>,
+) -> Result<impl Responder, Box<dyn std::error::Error>> {
+	let ident = path.into_inner();
+	let ident = ident.split('/').collect::<Vec<_>>();
+	let ident = ident.first().unwrap();
+	let ident = ident.parse::<u32>().unwrap();
+	let mut blogs = blog_data.write().map_err(|_| "Failed to lock data")?;
+	if let Some(ref mut blog) = blogs.get_mut(&ident) {
+		let local_time = Local::now().format("%Y-%m-%d %H:%M:%S").to_string();
+		println!("{local_time}: [blog]  cache hit:  {ident}");
+		blog.requests += 1;
+		let users = user_data.read().map_err(|_| "Failed to lock data")?;
+		let user = if let Some(user) = users.get(&blog.author_id) {
+			user.clone()
+		} else {
+			request_user(blog.author_id, api).await?
+		};
+		Ok(HttpResponse::Ok()
+			.content_type("text/html; charset=utf-8")
+			.body(blog_template(blog, &user)))
+	} else {
+		drop(blogs);
+		let local_time = Local::now().format("%Y-%m-%d %H:%M:%S").to_string();
+		println!("{local_time}: [blog]  cache miss: {ident}");
+		let blog = request_blog(ident, api.clone()).await?;
+		let users = user_data.read().map_err(|_| "Failed to lock data")?;
+		let user = if let Some(user) = users.get(&blog.author_id) {
+			user.clone()
+		} else {
+			request_user(blog.author_id, api).await?
+		};
+		Ok(HttpResponse::Ok()
+			.content_type("text/html; charset=utf-8")
+			.body(blog_template(&blog, &user)))
 	}
 }
 
@@ -217,6 +254,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
 	let users = Arc::new(RwLock::new(HashMap::<u32, User>::new()));
 	let users_clone = users.clone();
+
+	let blogs = Arc::new(RwLock::new(HashMap::<u32, Blog>::new()));
+	let blogs_clone = blogs.clone();
 
 	// Seconds between garbage collection.
 	const GC: u64 = 3600;
@@ -245,7 +285,17 @@ async fn main() -> Result<(), Box<dyn Error>> {
 			let remaining = users.len();
 			let dropped = count - remaining;
 			println!(
-				"{local_time}: [user]  Requests - {requests}, Stories - {count}, Dropped - {dropped}, Remaining - {remaining}"
+				"{local_time}: [user]  Requests - {requests}, Users   - {count}, Dropped - {dropped}, Remaining - {remaining}"
+			);
+
+			let mut blogs = blogs_clone.write().expect("Failed to lock data");
+			let count = blogs.len();
+			let requests = blogs.iter().map(|(_, blog)| blog.requests).sum::<u32>();
+			blogs.retain(|_, blog| blog.timestamp + TTL > time);
+			let remaining = blogs.len();
+			let dropped = count - remaining;
+			println!(
+				"{local_time}: [blog]  Requests - {requests}, Blogs   - {count}, Dropped - {dropped}, Remaining - {remaining}"
 			);
 		}
 	});
@@ -255,6 +305,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
 			.app_data(Data::new(api.clone()))
 			.app_data(Data::new(stories.clone()))
 			.app_data(Data::new(users.clone()))
+			.app_data(Data::new(blogs.clone()))
 			.wrap(
 				Cors::default()
 					.allow_any_origin()
@@ -265,6 +316,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
 			.service(get_story)
 			.service(oembed_story)
 			.service(get_user)
+			.service(get_blog)
 	})
 	.bind(("0.0.0.0", 7669))? // pony
 	.run()
@@ -333,6 +385,69 @@ async fn sleep(
 
 fn unix_time() -> Result<u128, Box<dyn std::error::Error + Send + Sync>> {
 	Ok(SystemTime::now().duration_since(UNIX_EPOCH)?.as_millis())
+}
+
+async fn request_user(
+	id: u32, api: web::Data<Arc<FimficRequest>>,
+) -> Result<User, Box<dyn std::error::Error>> {
+	let fimfic = format!("https://www.fimfiction.net/api/v2/users/{id}");
+	let response = handle_request(&api, &fimfic).await.unwrap();
+	let api = response.json::<UserApi>().await.unwrap();
+	let image = (!api.data.attributes.avatar.r64.ends_with("none_64.png"))
+		.then_some(api.data.attributes.avatar.r256);
+	let re = LazyLock::new(|| Regex::new(r"\[[^]]+\]").unwrap());
+	let user = User {
+		link: api.data.meta.url,
+		name: api.data.attributes.name.replace('"', "&quot;"),
+		requests: 1,
+		color: api.data.attributes.color.hex,
+		timestamp: unix_time().unwrap(),
+		bio_bbcode: re
+			.replace_all(&api.data.attributes.bio, "")
+			.to_string()
+			.replace('"', "&quot;"),
+		profile_pic_256_url: image,
+	};
+	Ok(user)
+}
+
+async fn request_blog(
+	id: u32, api: web::Data<Arc<FimficRequest>>,
+) -> Result<Blog, Box<dyn std::error::Error>> {
+	// title,intro,date_posted,content,content_html,num_views,num_comments,site_post,site_post_tag,tags,author,tagged_story
+	let fimfic = format!(
+		"https://www.fimfiction.net/api/v2/blog-posts/{id}?fields[blog_post]=title,date_posted,content,author,tagged_story"
+	);
+	let response = handle_request(&api, &fimfic).await.unwrap();
+	let api = response.json::<BlogApi>().await?;
+	let story_id = (api.data.relationships.tagged_story.data.id != "0")
+		.then_some(api.data.relationships.tagged_story.data.id.parse()?);
+	let re = LazyLock::new(|| Regex::new(r"\[[^]]+\]").unwrap());
+	let content = re
+		.replace_all(&api.data.attributes.content, "")
+		.to_string()
+		.replace('"', "&quot;");
+	let mut text = vec![];
+	let mut chars = 0;
+	for line in content.lines() {
+		if chars + line.len() < 512 - 1 {
+			text.push(line);
+			chars += line.len();
+		} else {
+			break;
+		}
+	}
+	let blog = Blog {
+		link: api.data.meta.url,
+		title: api.data.attributes.title,
+		requests: 1,
+		author_id: api.data.relationships.author.data.id.parse()?,
+		timestamp: unix_time().unwrap(),
+		story_id,
+		content_bbcode: text.join("\n"),
+		date_published: api.data.attributes.date_posted,
+	};
+	Ok(blog)
 }
 
 fn story_template(story: &Story) -> String {
@@ -445,6 +560,65 @@ fn user_template(user: &User) -> String {
 			user.bio_bbcode,
 			name = user.name,
 			link = user.link
+		),
+	}
+}
+
+fn blog_template(blog: &Blog, user: &User) -> String {
+	match &user.profile_pic_256_url {
+		Some(image) => format!(
+			r#"<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta name="theme-color" content="\#{}" />
+		<link rel="canonical" href="{link}" />
+		<meta http-equiv="refresh" content="0;url={link}" />
+		<meta property="og:title" content="{}" />
+		<meta property="og:description" content="{}" />
+		<meta property="og:image" content="{}" />
+		<meta property="og:url" content="{link}" />
+		<meta property="og:type" content="article" />
+		<meta property="article:author" content="{}" />
+		<meta property="article:published_time" content="{}" />
+		<meta property="og:site_name" content="Fimfiction" />
+		<meta property="twitter:site" content="fimfiction" />
+		<meta property="twitter:card" content="summary" />
+	</head>
+	<body></body>
+	</html>"#,
+			user.color,
+			blog.title,
+			blog.content_bbcode,
+			image,
+			user.link,
+			blog.date_published,
+			link = blog.link
+		),
+		None => format!(
+			r#"<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta name="theme-color" content="\#{}" />
+		<link rel="canonical" href="{link}" />
+		<meta http-equiv="refresh" content="0;url={link}" />
+		<meta property="og:title" content="{}" />
+		<meta property="og:description" content="{}" />
+		<meta property="og:url" content="{link}" />
+		<meta property="og:type" content="article" />
+		<meta property="article:author" content="{}" />
+		<meta property="article:published_time" content="{}" />
+		<meta property="og:site_name" content="Fimfiction" />
+		<meta property="twitter:site" content="fimfiction" />
+		<meta property="twitter:card" content="summary" />
+	</head>
+	<body></body>
+	</html>"#,
+			user.color,
+			blog.title,
+			blog.content_bbcode,
+			user.link,
+			blog.date_published,
+			link = blog.link
 		),
 	}
 }
