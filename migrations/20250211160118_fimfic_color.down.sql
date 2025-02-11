8e1c5cb63cbd70b25234a27169c3c20dfd1aea2a
Silk Rose <silkrose@love-tolerance.com>
2025-02-11 11:08:25 -0500

    Add FIMFiction embed color migration

---
+++ migrations/20250211160118_fimfic_color.down.sql
@@ -0,0 +1,1 @@
+-- Empty to make up work correctly.
---
+++ migrations/20250211160118_fimfic_color.up.sql
@@ -0,0 +1,7 @@
+INSERT INTO Colors
+	(name, color)
+	-- Embed colors from light and dark themes on Fimfiction.
+	-- Because the default is light, I don't think dark was ever seen on embeds.
+VALUES
+	('light', '56524c'),
+	('dark', '1d232b');
