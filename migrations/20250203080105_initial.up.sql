41715a1ad0cd4285e69f5a5990e70739a36f67d2
Silk Rose <silkrose@love-tolerance.com>
2025-02-13 05:50:02 -0500

    Add/update date rows to tables in initial SQL migration

--- migrations/20250203080105_initial.up.sql
+++ migrations/20250203080105_initial.up.sql
@@ -21,6 +21,7 @@ CREATE TABLE IF NOT EXISTS Authors (
 	blogs           integer     NOT NULL,
 	profile_pic_256 text        NULL,
 	color_hex       char(6)     NOT NULL,
+	date_joined     timestamptz NOT NULL,
 	date_cached     timestamptz NOT NULL DEFAULT now()
 );
 
@@ -39,6 +40,7 @@ CREATE TABLE IF NOT EXISTS Stories (
 	likes             integer           NOT NULL,
 	dislikes          integer           NOT NULL,
 	author_id         integer           NOT NULL,
+	date_published    timestamptz       NOT NULL,
 	date_cached       timestamptz       NOT NULL DEFAULT now(),
 
 	CONSTRAINT stories_author_id_fk FOREIGN KEY (author_id)
@@ -46,13 +48,14 @@ CREATE TABLE IF NOT EXISTS Stories (
 );
 
 CREATE TABLE IF NOT EXISTS Chapters (
-	story_id    integer     NOT NULL,
-	chapter_num integer     NOT NULL,
-	title       text        NOT NULL,
-	content     text        NOT NULL,
-	views       integer     NOT NULL,
-	words       integer     NOT NULL,
-	date_cached timestamptz NOT NULL DEFAULT now(),
+	story_id       integer     NOT NULL,
+	chapter_num    integer     NOT NULL,
+	title          text        NOT NULL,
+	content        text        NOT NULL,
+	views          integer     NOT NULL,
+	words          integer     NOT NULL,
+	date_published timestamptz NOT NULL,
+	date_cached    timestamptz NOT NULL DEFAULT now(),
 
 	CONSTRAINT chapter_story_id_fk FOREIGN KEY (story_id)
 		REFERENCES Stories (id) ON DELETE CASCADE,
@@ -61,14 +64,14 @@ CREATE TABLE IF NOT EXISTS Chapters (
 );
 
 CREATE TABLE IF NOT EXISTS Blogs (
-	id             integer  NOT NULL PRIMARY KEY,
-	title          text     NOT NULL,
-	content        text     NOT NULL,
-	comments       integer  NOT NULL,
-	views          integer  NOT NULL,
-	author_id      integer  NOT NULL,
-	story_id       integer  NULL,
-	date_published text     NOT NULL,
+	id          integer     NOT NULL PRIMARY KEY,
+	title       text        NOT NULL,
+	content     text        NOT NULL,
+	comments    integer     NOT NULL,
+	views       integer     NOT NULL,
+	author_id   integer     NOT NULL,
+	story_id    integer     NULL,
+	date_posted timestamptz NOT NULL,
 	date_cached timestamptz NOT NULL DEFAULT now(),
 
 	CONSTRAINT blogs_author_id_fk FOREIGN KEY (author_id)
