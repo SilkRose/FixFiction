487a12ef12c9a9d3eafea0de838c8f597c00ea6c
Ashy <meowy1227@gmail.com>
2025-04-04 00:52:10 -0400

    Add font-awesome-extractor

---
+++ scripts/font-awesome-extractor/extract.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+if [ ! -f FontAwesome.tar.gz ]; then
+	wget "https://github.com/Rush/Font-Awesome-SVG-PNG/archive/refs/tags/1.1.5.tar.gz" -O FontAwesome.tar.gz || {
+		echo "Failed to download FontAwesome."
+		exit 1
+	}
+fi
+if [ ! -d FontAwesome/ ]; then
+	mkdir -p FontAwesome/
+	tar -xvzf FontAwesome.tar.gz --strip-components=1 -C FontAwesome/ || {
+		echo "Failed to extract FontAwesome."
+		exit 1
+	}
+fi
+
+# cat icons.html | grep -oP '(?<=data-icon-type="font-awesome" class="bookshelf-icon-element fa fa-).*?(?="></span>)' | tr '\n' ' '
+icons="bolt book bookmark certificate check times clock-o cog exclamation eye film gamepad globe heart lock music road search star thumbs-up thumbs-down thumb-tack smile-o meh-o frown-o trash-o user youtube-play "
+
+mkdir -p Out/
+for icon in $icons; do
+	cp -a "FontAwesome/white/svg/$icon.svg" "Out/fa-$icon.svg"
+done
