f9c65d3f28d2a8715be5c270e5ac00eff43ecc17
Silk Rose <silkrose@love-tolerance.com>
2025-04-11 01:39:16 -0400

    Add tag and tag link tables to database

--- migrations/20250203080105_initial.up.sql
+++ migrations/20250203080105_initial.up.sql
@@ -16,6 +16,16 @@ CREATE TYPE authors_note_pos AS enum (
 	'bottom'
 );
 
+CREATE TYPE tag_type AS enum (
+	'character',
+	'genre',
+	'rating',
+	'content',
+	'series',
+	'warning',
+	'universe'
+);
+
 CREATE TABLE IF NOT EXISTS Authors (
 	id              integer     NOT NULL PRIMARY KEY,
 	name            text        NOT NULL,
@@ -95,4 +105,27 @@ CREATE TABLE IF NOT EXISTS Blogs (
 
 	CONSTRAINT blogs_story_id_fk FOREIGN KEY (story_id)
 		REFERENCES Stories (id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS Tags (
+	id          integer     NOT NULL PRIMARY KEY,
+	name        text        NOT NULL,
+	type        tag_type    NOT NULL,
+	description text        NULL,
+	old_id      text        NULL,
+	link        text        NOT NULL,
+	date_cached timestamptz NOT NULL DEFAULT now()
+);
+
+CREATE TABLE IF NOT EXISTS Tag_links (
+	story_id integer,
+	tag_id   integer,
+
+	CONSTRAINT tag_links_story_id_fk FOREIGN KEY (story_id)
+		REFERENCES Stories (id) ON DELETE CASCADE,
+
+	CONSTRAINT tag_links_tag_id_fk FOREIGN KEY (tag_id)
+		REFERENCES Tags (id) ON DELETE CASCADE,
+
+	CONSTRAINT tag_links_pk PRIMARY KEY (story_id, tag_id)
 );
