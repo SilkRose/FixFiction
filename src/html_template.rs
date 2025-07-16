357786e9a51e351ecef3a2b0ff55622b2e7ee2ea
Silk Rose <silkrose@love-tolerance.com>
2025-07-16 13:18:18 -0400

    Replace string concatenation with Maud HTML template

--- src/html_template.rs
+++ src/html_template.rs
@@ -1,82 +1,76 @@
 use crate::structs::EmbedData;
+use maud::{DOCTYPE, PreEscaped, html};
 use url::form_urlencoded;
 
 pub fn embed_html_template(embed: EmbedData) -> String {
-	let mut text = String::new();
-	text.push_str(r#"<!DOCTYPE html><html lang="en"><head>"#);
-	text.push_str("<!-- FixFiction: https://github.com/SilkRose/FixFiction -->");
-	text.push_str("<!-- Pinkie Pie is best pony! -->");
-	if let Some(comment) = embed.html_comment {
-		text.push_str(&format!("<!-- {comment} -->"));
+	html! {
+		(DOCTYPE) html lang = "en" {
+			head {
+				(PreEscaped ("<!-- FixFiction: https://github.com/SilkRose/FixFiction -->"));
+				(PreEscaped ("<!-- Pinkie Pie is best pony! -->"));
+				@if let Some(comment) = embed.html_comment {
+					(PreEscaped (format!("<!-- {comment} -->")));
+				}
+				@if let Some(color) = embed.color {
+					meta name = "theme-color" content = { "#" (color) } "/";
+				}
+				link rel = "canonical" href = (embed.link) "/";
+				meta http-equiv = "refresh" content={ "0;url=" (embed.link) } "/";
+				meta property = "og:title" content = (embed.title) "/";
+				meta property = "og:description" content = (embed.description) "/";
+				@if let Some(cover) = embed.cover {
+					meta property = "og:image" content = (cover) "/";
+				}
+				meta property = "og:url" content = (embed.link) "/";
+				meta property = "og:type" content = (embed.open_graph_type) "/";
+				@if let Some(property) = embed.open_graph_property {
+					@if let Some(ref username) = embed.user_name {
+						meta property = (property) content = (username) "/";
+					}
+				};
+				@let site_name = if !embed.errors.is_empty() {
+					format!("{}\n{}", embed.site_name, embed.errors)
+				} else {
+					embed.site_name
+				};
+				meta property = "og:site_name" content = (site_name) "/";
+				meta property = "twitter:site" content = "fimfiction" "/";
+				meta property = "twitter:card" content = "summary" "/";
+				@let encode = encode_url(
+					&site_name,
+					&embed.site_url,
+					&embed.title,
+					embed.user_name,
+					embed.user_link);
+				link
+					rel = "alternate"
+					type = "application/json+oembed"
+					href = { "https://www.fixfiction.net/oembed?" (encode) }
+					title = (embed.title) "/";
+				};
+			body {};
+		};
 	}
-	if let Some(color) = embed.color {
-		text.push_str(&format!(
-			r##"<meta name="theme-color" content="#{color}" />"##
-		));
-	}
-	text.push_str(&format!(
-		r#"<link rel="canonical" href="{}" />"#,
-		embed.link
-	));
-	text.push_str(&format!(
-		r#"<meta http-equiv="refresh" content="0;url={}" />"#,
-		embed.link
-	));
-	text.push_str(&format!(
-		r#"<meta property="og:title" content="{}" />"#,
-		embed.title
-	));
-	text.push_str(&format!(
-		r#"<meta property="og:description" content="{}" />"#,
-		embed.description
-	));
-	if let Some(cover) = embed.cover {
-		text.push_str(&format!(
-			r#"<meta property="og:image" content="{cover}" />"#
-		));
-	}
-	text.push_str(&format!(
-		r#"<meta property="og:url" content="{}" />"#,
-		embed.link
-	));
-	text.push_str(&format!(
-		r#"<meta property="og:type" content="{}" />"#,
-		embed.open_graph_type
-	));
-	if let Some(property) = embed.open_graph_property {
-		if let Some(ref username) = embed.user_name {
-			text.push_str(&format!(
-				r#"<meta property="{property}" content="{username}" />"#
-			))
-		}
-	};
-	let site_name = if !embed.errors.is_empty() {
-		format!("{}\n{}", embed.site_name, embed.errors)
-	} else {
-		embed.site_name
-	};
-	text.push_str(&format!(
-		r#"<meta property="og:site_name" content="{site_name}" />"#
-	));
-	text.push_str(r#"<meta property="twitter:site" content="fimfiction" />"#);
-	text.push_str(r#"<meta property="twitter:card" content="summary" />"#);
+	.into()
+}
+
+fn encode_url(
+	site_name: &str, site_url: &str, title: &str, username: Option<String>,
+	user_link: Option<String>,
+) -> String {
 	let mut encode = form_urlencoded::Serializer::new(String::new());
 	encode.append_pair("type", "rich");
 	encode.append_pair("version", "1");
-	encode.append_pair("provider_name", &site_name);
-	encode.append_pair("provider_url", &embed.site_url);
-	encode.append_pair("title", &embed.title);
-	if let Some(ref username) = embed.user_name {
+	encode.append_pair("provider_name", site_name);
+	encode.append_pair("provider_url", site_url);
+	encode.append_pair("title", title);
+	if let Some(ref username) = username {
 		encode.append_pair("author_name", username);
 	}
-	if let Some(ref user_link) = embed.user_link {
+	if let Some(ref user_link) = user_link {
 		encode.append_pair("author_url", user_link);
 	}
 	encode.append_pair("cache_age", "86400");
 	encode.append_pair("html", "");
-	let encode = encode.finish();
-	text.push_str(&format!(
-		r#"<link rel="alternate" type="application/json+oembed" href="https://www.fixfiction.net/oembed?{encode}" title="{}" />"#, embed.title));
-	text.push_str(r#"</head><body></body></html>"#);
-	text
+	encode.finish()
 }
