a9ae289ddc6bc582324b0d6904701294bb98c421
Silk Rose <silkrose@love-tolerance.com>
2025-07-16 04:55:07 -0400

    Use HTML embed template for user embeds

--- src/user.rs
+++ src/user.rs
@@ -1,12 +1,12 @@
 use crate::check_recache;
 use crate::database::{get_user, insert_user};
 use crate::fimfiction_api::user::UserApi;
-use crate::structs::{AppState, Color, Cover, Parameters, User};
+use crate::html_template::embed_html_template;
+use crate::structs::{AppState, Color, Cover, EmbedData, Parameters, User};
 use crate::utility::{get_color, map_picture, parse_fimfic_response};
 use chrono::{TimeDelta, Utc};
 use pony::number_format::{FormatType, format_number_unit_metric};
 use std::error::Error;
-use url::form_urlencoded;
 
 pub async fn request_user(id: i32, app: &AppState, recache: bool) -> Result<User, Box<dyn Error>> {
 	let user = get_user(id, &app.db).await?;
@@ -24,10 +24,6 @@ pub async fn request_user(id: i32, app: &AppState, recache: bool) -> Result<User
 pub fn user_html_template(
 	user: User, parameters: Parameters, link: String, errors: String,
 ) -> String {
-	let mut text = String::new();
-	text.push_str(r#"<!DOCTYPE html><html lang="en"><head>"#);
-	text.push_str("<!-- FixFiction: https://github.com/SilkRose/FixFiction -->");
-	text.push_str("<!-- Pinkie Pie is best pony! -->");
 	let color = match parameters.color {
 		Some(color) => match color {
 			Color::None => None,
@@ -38,23 +34,6 @@ pub fn user_html_template(
 		},
 		None => Some(user.color_hex),
 	};
-	if let Some(color) = color {
-		text.push_str(&format!(
-			r##"<meta name="theme-color" content="#{color}" />"##
-		));
-	}
-	text.push_str(&format!(r#"<link rel="canonical" href="{link}" />"#));
-	text.push_str(&format!(
-		r#"<meta http-equiv="refresh" content="0;url={link}" />"#
-	));
-	text.push_str(&format!(
-		r#"<meta property="og:title" content="{}" />"#,
-		user.name
-	));
-	text.push_str(&format!(
-		r#"<meta property="og:description" content="{}" />"#,
-		user.bio
-	));
 	let cover = match parameters.cover {
 		Some(cover) => match cover {
 			Cover::None => None,
@@ -63,18 +42,7 @@ pub fn user_html_template(
 		},
 		None => map_picture(user.profile_pic_url),
 	};
-	if let Some(cover) = cover {
-		text.push_str(&format!(
-			r#"<meta property="og:image" content="{cover}" />"#
-		));
-	}
-	text.push_str(&format!(r#"<meta property="og:url" content="{link}" />"#));
-	text.push_str(r#"<meta property="og:type" content="profile" />"#);
-	text.push_str(&format!(
-		r#"<meta property="profile:username" content="{}" />"#,
-		user.name
-	));
-	let mut site_name = if parameters.stats {
+	let site_name = if parameters.stats {
 		{
 			let time = user.date_joined.format("%a %b %e %Y").to_string();
 			format!(
@@ -89,26 +57,20 @@ pub fn user_html_template(
 	} else {
 		"Fimfiction".to_string()
 	};
-	if !errors.is_empty() {
-		site_name = format!("{site_name}\n{errors}");
-	}
-	text.push_str(&format!(
-		r#"<meta property="og:site_name" content="{site_name}" />"#
-	));
-	text.push_str(r#"<meta property="twitter:site" content="fimfiction" />"#);
-	text.push_str(r#"<meta property="twitter:card" content="summary" />"#);
-	let mut encode = form_urlencoded::Serializer::new(String::new());
-	encode.append_pair("type", "rich");
-	encode.append_pair("version", "1");
-	encode.append_pair("provider_name", &site_name);
-	encode.append_pair("provider_url", "https://www.fimfiction.net/");
-	encode.append_pair("title", &user.name);
-	encode.append_pair("cache_age", "86400");
-	encode.append_pair("html", "");
-	let encode = encode.finish();
-	text.push_str(&format!(
-			r#"<link rel="alternate" type="application/json+oembed" href="https://www.fixfiction.net/oembed?{encode}" title="{}" />"#,
-		user.name));
-	text.push_str(r#"</head><body></body></html>"#);
-	text
+	let data = EmbedData {
+		title: user.name,
+		description: user.bio,
+		link,
+		color,
+		cover,
+		site_name,
+		site_url: String::from("https://www.fimfiction.net/"),
+		errors,
+		user_name: None,
+		user_link: None,
+		html_comment: None,
+		open_graph_type: String::from("profile"),
+		open_graph_property: Some(String::from("profile:username")),
+	};
+	embed_html_template(data)
 }
