0f7d9d16daf7aad3292b515f426e9b9d384f8230
Silk Rose <silkrose@love-tolerance.com>
2025-07-16 05:52:05 -0400

    Use HTML embed template for group embeds

--- src/group.rs
+++ src/group.rs
@@ -1,14 +1,14 @@
 use crate::database::{get_group, insert_group, insert_user};
 use crate::fimfiction_api::ApiIncluded;
 use crate::fimfiction_api::group::GroupApi;
-use crate::structs::{AppState, Color, Cover, Group, Parameters, User};
+use crate::html_template::embed_html_template;
+use crate::structs::{AppState, Color, Cover, EmbedData, Group, Parameters, User};
 use crate::user::request_user;
 use crate::utility::{get_color, map_picture, parse_fimfic_response};
 use crate::{check_recache, get_variant};
 use chrono::{TimeDelta, Utc};
 use pony::number_format::{FormatType, format_number_unit_metric};
 use std::error::Error;
-use url::form_urlencoded;
 
 pub async fn request_group(
 	id: i32, app: &AppState, recache: bool,
@@ -35,14 +35,10 @@ pub async fn request_group(
 pub fn group_html_template(
 	group: Group, founder: User, parameters: Parameters, link: String, errors: String,
 ) -> String {
-	let mut text = String::new();
-	let founder_link = match parameters.stats {
+	let founder_name = match parameters.stats {
 		true => Some(format!("Founder: {}", founder.name)),
 		false => None,
 	};
-	text.push_str(r#"<!DOCTYPE html><html lang="en"><head>"#);
-	text.push_str("<!-- FixFiction: https://github.com/SilkRose/FixFiction -->");
-	text.push_str("<!-- Pinkie Pie is best pony! -->");
 	let color = match parameters.color {
 		Some(color) => match color {
 			Color::None => None,
@@ -60,23 +56,6 @@ pub fn group_html_template(
 			None => Some(get_color(Some(group.id))),
 		},
 	};
-	if let Some(color) = color {
-		text.push_str(&format!(
-			r##"<meta name="theme-color" content="#{color}" />"##
-		));
-	}
-	text.push_str(&format!(r#"<link rel="canonical" href="{link}" />"#));
-	text.push_str(&format!(
-		r#"<meta http-equiv="refresh" content="0;url={link}" />"#
-	));
-	text.push_str(&format!(
-		r#"<meta property="og:title" content="{}" />"#,
-		group.name
-	));
-	text.push_str(&format!(
-		r#"<meta property="og:description" content="{}" />"#,
-		group.description
-	));
 	let cover = match parameters.cover {
 		Some(cover) => match cover {
 			Cover::User | Cover::Story => map_picture(founder.profile_pic_url),
@@ -84,19 +63,7 @@ pub fn group_html_template(
 		},
 		None => map_picture(group.icon_url).or(map_picture(founder.profile_pic_url)),
 	};
-	if let Some(cover) = cover {
-		text.push_str(&format!(
-			r#"<meta property="og:image" content="{cover}" />"#
-		));
-	}
-	text.push_str(&format!(r#"<meta property="og:url" content="{link}" />"#));
-	text.push_str(r#"<meta property="og:type" content="profile" />"#);
-	if let Some(ref founder_link) = founder_link {
-		text.push_str(&format!(
-			r#"<meta property="profile:username" content="{founder_link}" />"#,
-		));
-	}
-	let mut site_name = if parameters.stats {
+	let site_name = if parameters.stats {
 		let time = group.date_created.format("%a %b %e %Y").to_string();
 		let mature = match group.nsfw {
 			true => "Not safe for work: 🔞",
@@ -114,30 +81,20 @@ pub fn group_html_template(
 	} else {
 		"Fimfiction".to_string()
 	};
-	if !errors.is_empty() {
-		site_name = format!("{site_name}\n{errors}");
-	}
-	text.push_str(&format!(
-		r#"<meta property="og:site_name" content="{site_name}" />"#
-	));
-	text.push_str(r#"<meta property="twitter:site" content="fimfiction" />"#);
-	text.push_str(r#"<meta property="twitter:card" content="summary" />"#);
-	let mut encode = form_urlencoded::Serializer::new(String::new());
-	encode.append_pair("type", "rich");
-	encode.append_pair("version", "1");
-	encode.append_pair("provider_name", &site_name);
-	encode.append_pair("provider_url", "https://www.fimfiction.net/");
-	encode.append_pair("title", &group.name);
-	if let Some(founder_link) = founder_link {
-		encode.append_pair("author_name", &founder_link);
-		encode.append_pair("author_url", &founder.link);
-	}
-	encode.append_pair("cache_age", "86400");
-	encode.append_pair("html", "");
-	let encode = encode.finish();
-	text.push_str(&format!(
-		r#"<link rel="alternate" type="application/json+oembed" href="https://www.fixfiction.net/oembed?{encode}" title="{}" />"#,
-		&group.name));
-	text.push_str(r#"</head><body></body></html>"#);
-	text
+	let data = EmbedData {
+		title: group.name,
+		description: group.description,
+		link,
+		color,
+		cover,
+		site_name,
+		site_url: String::from("https://www.fimfiction.net/"),
+		errors,
+		user_name: founder_name,
+		user_link: Some(founder.link),
+		html_comment: None,
+		open_graph_type: String::from("profile"),
+		open_graph_property: Some(String::from("profile:username")),
+	};
+	embed_html_template(data)
 }
