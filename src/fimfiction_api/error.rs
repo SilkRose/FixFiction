ea9729dbcde5bab878df577c65126286d053d762
PseudoBob-Delightus <159068625+PseudoBob-Delightus@users.noreply.github.com>
2026-05-20 21:07:24 -0400

    Add fimfiction api / error module docs

--- src/fimfiction_api/error.rs
+++ src/fimfiction_api/error.rs
@@ -1,10 +1,12 @@
 use serde::{Deserialize, Serialize};
 
+/// A list of errors returned by the API.
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub struct FimficError<T = u32> {
 	pub errors: Vec<FimficErrorInner<T>>,
 }
 
+/// A full individual error object.
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub struct FimficErrorInner<T = u32> {
 	pub status: String,
@@ -15,12 +17,16 @@ pub struct FimficErrorInner<T = u32> {
 	pub links: ErrorLinks,
 }
 
+/// Properties of a potential resource related to an error.
+/// 
+/// The resource may not exist if, for example, the error was "Resource not found".
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub struct ErrorMeta<T = u32> {
 	pub resource: String,
 	pub id: OptionsID<T>,
 }
 
+/// The ID field of a potential resource. The API may return it as a number or a string.
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
 #[serde(untagged)]
 pub enum OptionsID<T> {
@@ -28,6 +34,7 @@ pub enum OptionsID<T> {
 	String(String),
 }
 
+/// Links provided by the API to add context to the error.
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub struct ErrorLinks {
 	pub about: String,
