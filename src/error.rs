b5d7ee079e59341bc6e07f108b4aa20f2f7a5ea4
Silk Rose <silkrose@love-tolerance.com>
2026-06-12 01:15:39 -0400

    Implement better actix errors
    Not working yet, but maybe soon?

--- src/error.rs
+++ src/error.rs
@@ -2,11 +2,60 @@
 
 use crate::html_template::{EmbedData, embed_html_template};
 use crate::utility::LOG;
+use actix_web::http::StatusCode;
+use actix_web::http::header::ContentType;
+use actix_web::{HttpResponse, ResponseError};
 use std::error::Error as StdError;
+use std::fmt::Display;
 use std::result::Result as StdResult;
 
 pub(crate) type Error = Box<dyn StdError>;
 pub(crate) type Result<T, E = Error> = StdResult<T, E>;
+
+pub(crate) trait EmbedError<T> {
+	fn map_embed_err(self, endpoint: &str, link: &str) -> Result<T>;
+}
+
+impl<T> EmbedError<T> for Result<T> {
+	fn map_embed_err(self, endpoint: &str, link: &str) -> Result<T> {
+		if let Err(ref error) = self {
+			let body = error_html_template(endpoint, link.to_string(), error.to_string());
+			return Err(Box::new(EmbedErrorType::new(body)));
+		}
+		self
+	}
+}
+
+#[derive(Debug)]
+pub(crate) struct EmbedErrorType {
+	inner: String,
+}
+
+impl EmbedErrorType {
+	pub fn new(body: String) -> Self {
+		EmbedErrorType { inner: body }
+	}
+}
+
+impl std::error::Error for EmbedErrorType {}
+
+impl Display for EmbedErrorType {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		f.write_str(&self.inner)
+	}
+}
+
+impl ResponseError for EmbedErrorType {
+	fn status_code(&self) -> StatusCode {
+		StatusCode::OK
+	}
+
+	fn error_response(&self) -> HttpResponse {
+		HttpResponse::Ok()
+			.content_type("text/html; charset=utf-8")
+			.body(self.to_string())
+	}
+}
 
 /// Formats errors to an HTML string for embedding.
 pub(crate) fn error_html_template(endpoint: &str, link: String, errors: String) -> String {
