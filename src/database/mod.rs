00b3572bc2ab045d5c2c3821339eb5d086a52c32
Silk Rose <silkrose@love-tolerance.com>
2026-06-19 19:09:31 -0400

    Move tag database code into submodule

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -10,6 +10,7 @@ mod bookshelf;
 mod chapter;
 mod group;
 mod story;
+mod tag;
 mod thread;
 mod user;
 
@@ -27,47 +28,6 @@ impl Db {
 			.await?;
 		sqlx::migrate!().run(&pool).await?;
 		Ok(Self { pool })
-	}
-
-	/// Selects a [Tag] from the database
-	pub(crate) async fn get_tag(&self, id: i32) -> Result<Option<Tag>> {
-		sqlx::query_as!(
-			Tag,
-			r#"SELECT
-				id, name, type AS "tag_type: TagType", old_id, link, date_cached
-			FROM Tags
-			WHERE id = $1
-			LIMIT 1;"#,
-			id
-		)
-		.fetch_optional(&self.pool)
-		.await
-		.map_err(db_select_err)
-	}
-
-	/// Inserts a [Tag] into the database
-	pub(crate) async fn insert_tag(&self, tag: &Tag) -> Result<PgQueryResult> {
-		sqlx::query!(
-			r#"INSERT INTO Tags
-				(id, name, type, old_id, link, date_cached)
-			VALUES
-				($1, $2, $3, $4, $5, $6)
-			ON CONFLICT(id) DO UPDATE SET
-				name = EXCLUDED.name,
-				type = EXCLUDED.type,
-				old_id = EXCLUDED.old_id,
-				link = EXCLUDED.link,
-				date_cached = EXCLUDED.date_cached;"#,
-			tag.id,
-			tag.name,
-			tag.tag_type as _,
-			tag.old_id,
-			tag.link,
-			tag.date_cached,
-		)
-		.execute(&self.pool)
-		.await
-		.map_err(db_insert_err)
 	}
 
 	/// Selects [TagLink]s from the database for a given story ID
