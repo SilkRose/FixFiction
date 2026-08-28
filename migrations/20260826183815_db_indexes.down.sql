6b17cf045d54200adc31f1bc8eee339d86d05c03
Silk Rose <silkrose@love-tolerance.com>
2026-08-28 07:56:21 -0400

    Add database index SQL migrations

---
+++ migrations/20260826183815_db_indexes.down.sql
@@ -0,0 +1,11 @@
+ALTER TABLE Groups DROP CONSTRAINT group_index_fk;
+DROP TABLE IF EXISTS group_index;
+ALTER TABLE Blogs DROP CONSTRAINT blog_index_fk;
+DROP TABLE IF EXISTS Blog_index;
+ALTER TABLE Stories DROP CONSTRAINT story_index_fk;
+DROP TABLE IF EXISTS Story_index;
+ALTER TABLE Authors DROP CONSTRAINT author_index_fk;
+DROP TABLE IF EXISTS Author_index;
+
+DROP TYPE IF EXISTS fimfic_status;
+DROP TYPE IF EXISTS archive_status;
---
+++ migrations/20260826183815_db_indexes.up.sql
@@ -0,0 +1,43 @@
+CREATE TYPE archive_status AS enum (
+	'un_archived',
+	'partially_archived',
+	'archived'
+);
+
+CREATE TYPE fimfic_status AS enum (
+	'live',
+	'deleted'
+);
+
+CREATE TABLE IF NOT EXISTS Author_index (
+	id             integer        NOT NULL PRIMARY KEY,
+	archive_status archive_status NOT NULL,
+	fimfic_status  fimfic_status  NOT NULL,
+	date_checked   timestamptz    NOT NULL DEFAULT now()
+);
+
+ALTER TABLE Authors
+	ADD CONSTRAINT author_index_fk FOREIGN KEY (id)
+		REFERENCES Author_index (id) ON DELETE CASCADE;
+
+CREATE TABLE IF NOT EXISTS Story_index (
+	id             integer        NOT NULL PRIMARY KEY,
+	archive_status archive_status NOT NULL,
+	fimfic_status  fimfic_status  NOT NULL,
+	date_checked   timestamptz    NOT NULL DEFAULT now()
+);
+
+ALTER TABLE Stories
+	ADD CONSTRAINT story_index_fk FOREIGN KEY (id)
+		REFERENCES Story_index (id) ON DELETE CASCADE;
+
+CREATE TABLE IF NOT EXISTS group_index (
+	id             integer        NOT NULL PRIMARY KEY,
+	archive_status archive_status NOT NULL,
+	fimfic_status  fimfic_status  NOT NULL,
+	date_checked   timestamptz    NOT NULL DEFAULT now()
+);
+
+ALTER TABLE Groups
+	ADD CONSTRAINT group_index_fk FOREIGN KEY (id)
+		REFERENCES Group_index (id) ON DELETE CASCADE;
