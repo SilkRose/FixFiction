0c8e5f72960126e0554464c25da2e5fd88e291d6
Silk Rose <silkrose@love-tolerance.com>
2026-08-29 01:19:42 -0400

    Update database index SQL migrations

--- migrations/20260826183815_db_indexes.up.sql
+++ migrations/20260826183815_db_indexes.up.sql
@@ -5,7 +5,7 @@ CREATE TYPE archive_status AS enum (
 );
 
 CREATE TYPE fimfic_status AS enum (
-	'live',
+	'public',
 	'deleted'
 );
 
@@ -30,6 +30,17 @@ CREATE TABLE IF NOT EXISTS Story_index (
 ALTER TABLE Stories
 	ADD CONSTRAINT story_index_fk FOREIGN KEY (id)
 		REFERENCES Story_index (id) ON DELETE CASCADE;
+
+CREATE TABLE IF NOT EXISTS Blog_index (
+	id             integer        NOT NULL PRIMARY KEY,
+	archive_status archive_status NOT NULL,
+	fimfic_status  fimfic_status  NOT NULL,
+	date_checked   timestamptz    NOT NULL DEFAULT now()
+);
+
+ALTER TABLE Blogs
+	ADD CONSTRAINT blog_index_fk FOREIGN KEY (id)
+		REFERENCES Blog_index (id) ON DELETE CASCADE;
 
 CREATE TABLE IF NOT EXISTS group_index (
 	id             integer        NOT NULL PRIMARY KEY,
